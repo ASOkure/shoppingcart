dc51e62386c51de57872a9301bd77c37b27e0118
Akanimo Sam Okure <akan.okure@gmail.com>
2024-06-20 12:49:43 +0100

    change usd symbol to  pounds

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -8,7 +8,7 @@ export const ProductCard = ({ product }) => {
       <img src={image} alt={name} />
       <p className="name">{name}</p>
       <div className="action">
-        <p>${price}</p>
+        <p>£{price}</p>
         <button>Add to Cart</button>
       </div>
     </div>
